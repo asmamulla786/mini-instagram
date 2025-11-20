948901c1b797879a4538069f121dca8880794d88
Asma <183721570+asmamulla786@users.noreply.github.com>
2025-11-20 07:33:19 +0530

    Created project repo and done all the setup

---
+++ src/test/java/com/projects/My_Instagram/MyInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.My_Instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
