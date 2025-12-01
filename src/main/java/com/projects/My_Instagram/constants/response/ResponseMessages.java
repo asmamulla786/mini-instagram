2498e2c13fc21678d5c5bfb7a93fbc225dd31c40
Asma <183721570+asmamulla786@users.noreply.github.com>
2025-12-01 12:24:49 +0530

    extracted response messages to ResponseMessages.java

--- src/main/java/com/projects/My_Instagram/constants/response/ResponseMessages.java
+++ src/main/java/com/projects/My_Instagram/constants/response/ResponseMessages.java
@@ -1,7 +1,12 @@
 package com.projects.My_Instagram.constants.response;
 
 public enum ResponseMessages {
-    USER_CREATED_SUCCESSFULLY("User Created Successfully");
+    USER_CREATED_SUCCESSFULLY("User Created Successfully"),
+    LIKED_SUCCESSFULLY("Post liked successfully"),
+    ALREADY_LIKED("Post liked successfully"),
+    NOT_LIKED("You haven't liked the post"),
+    UNLIKED_SUCCESSFULLY("Post unliked successfully"),
+    DELETED_COMMENT_SUCCESSFULLY("Deleted comment successfully");
     private String message;
 
     ResponseMessages(String message) {
